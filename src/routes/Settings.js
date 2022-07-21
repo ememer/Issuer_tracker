fcd88fcb8fabadc7f4ed421d82a72b5437c02f06
ememer <46853050+ememer@users.noreply.github.com>
2022-07-21 12:49:59 +0200

    Updated setinngs page

--- src/routes/Settings.js
+++ src/routes/Settings.js
@@ -45,93 +45,91 @@ const Settings = () => {
   return (
     <Layout>
       {avatarMenu && <AvatarPopup onClose={() => setAvatarMenu(false)} />}
-      <div className="grid-row user">
-        <Headline
-          level={2}
-          headlineBG={true}
-          title="Sekcja użytkownika"
-          className="grid-col-12 headline-size"
-        />
-
-        <div className="grid-col-3 avatar-section">
-          <div className="avatar-group">
-            <AvatarImage
-              photoAlt="User Photo"
-              src={displayAvatar(avatar, avatarLists).url}
-              style={displayAvatar(avatar, avatarLists).border}
-              className="avatar"
-            />
-            <button
-              onClick={() => setAvatarMenu(true)}
-              className="avatar-setting"
-              style={{
-                background: `${displayAvatar(avatar, avatarLists).border}`,
-              }}
-            >
-              <FontAwesomeIcon icon={faGear} />
-            </button>
-          </div>
-        </div>
-        <div className="grid-col-2 user-area">
-          <form onSubmit={(e) => setNameOfUser(e)}>
-            <label>
-              Twoja nazwa <span>*</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Wpisz swoją nazwę!"
-              value={userNameInput}
-              onChange={(e) => setUserNameInput(e.target.value)}
-            />
-            {userNameInput !== "" && userNameInput.length >= 3 && (
-              <div className="grid-row">
-                <div className="grid-col-12 form-button">
+      <div className="row">
+        <div className="col-lg-6">
+          <Headline
+            level={2}
+            headlineBG={true}
+            title="Sekcja użytkownika"
+            className="headline-size"
+          />
+          <div className="row avatar-section">
+            <div className="col-2 avatar-group">
+              <AvatarImage
+                photoAlt="User Photo"
+                src={displayAvatar(avatar, avatarLists).url}
+                style={displayAvatar(avatar, avatarLists).border}
+                className="avatar"
+              />
+              <button
+                onClick={() => setAvatarMenu(true)}
+                className="avatar-setting"
+                style={{
+                  background: `${displayAvatar(avatar, avatarLists).border}`,
+                }}
+              >
+                <FontAwesomeIcon icon={faGear} />
+              </button>
+            </div>
+            <div className="col-3 user-box">
+              <form onSubmit={(e) => setNameOfUser(e)}>
+                <label>
+                  Twoja nazwa <span>*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Wpisz swoją nazwę!"
+                  value={userNameInput}
+                  onChange={(e) => setUserNameInput(e.target.value)}
+                />
+              </form>
+              {userNameInput !== "" && userNameInput.length >= 3 && (
+                <div className="form-button">
                   <button onClick={(e) => setNameOfUser(e)}>Ustaw</button>
                 </div>
-              </div>
-            )}
-          </form>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="grid-row facilitation">
-        <Headline
-          level={2}
-          headlineBG={true}
-          title="Wielkość interface'u"
-          className="grid-col-12 headline-size"
-        />
-
-        <div className="grid-col-4">
-          <div className="grid-row sizes">
-            <button
-              onClick={() => {
-                if (uiSize > 1) {
-                  setUiSize((prevState) => --prevState);
-                }
-              }}
-              className="grid-col-2"
-            >
-              -A
-            </button>
-            <input
-              type="range"
-              step={1}
-              min={1}
-              max={3}
-              value={uiSize}
-              onChange={(e) => setUiSize(+e.target.value)}
-              className="grid-col-8"
-            />
-            <button
-              onClick={() => {
-                if (uiSize < 3) {
-                  setUiSize((prevState) => ++prevState);
-                }
-              }}
-              className="grid-col-2"
-            >
-              A+
-            </button>
+        <div className="col-lg-6">
+          <Headline
+            level={2}
+            headlineBG={true}
+            title="Wielkość interface'u"
+            className="headline-size"
+          />
+          <div className="col-12">
+            <div className="row sizes">
+              <button
+                onClick={() => {
+                  if (uiSize > 1) {
+                    setUiSize((prevState) => --prevState);
+                  }
+                }}
+                className="col-3"
+              >
+                -A
+              </button>
+              <input
+                type="range"
+                step={1}
+                min={1}
+                max={3}
+                value={uiSize}
+                onChange={(e) => setUiSize(+e.target.value)}
+                className="col-4"
+              />
+              <button
+                onClick={() => {
+                  if (uiSize < 3) {
+                    setUiSize((prevState) => ++prevState);
+                  }
+                }}
+                className="col-3"
+              >
+                A+
+              </button>
+            </div>
           </div>
         </div>
       </div>
